b6b1be640e073bbfd62b702d0590f8682ec6bbef
Robin60 <owinorobin05@gmail.com>
2023-08-06 20:50:53 +0000

    fix: Correction on tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Complex types - string and int/float to tuple
 function to_kv that takes a string k and an int OR float v as arguments
-and returns a tuple"""
+and returns a tuple
+"""
 
 
 from typing import Callable, Iterator, Union, Optional, List, Tuple
 
 
-def to_kv(k: str, Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
     """Takes a string k and an int OR float v as arguments and
     returns a tuple """
 
